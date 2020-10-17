eedc1d49a28792b03e7da1f1a07a0258f1180e4e
Ameen Kunnathu <akunnat1@binghamton.edu>
2020-10-16 22:01:27 -0400

    second commit

--- lab2.py
+++ lab2.py
@@ -1,57 +1,90 @@
-import turtle               #1. import modules
+import turtle              
 import random
 
-#Part A
-window = turtle.Screen()       # 2.  Create a screen
+window = turtle.Screen()       
 window.bgcolor('lightblue')
 
-michelangelo = turtle.Turtle()    # 3.  Create two turtles
+michelangelo = turtle.Turtle()   
 leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
 
-michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+def turtleSetUp():
+  '''
+   Sets up Turtles.
+   No args.
+   No return value.
+  '''
+ 
+  michelangelo.color('orange')
+  leonardo.color('blue')
 
-## 5. your code goes here
+  michelangelo.shape('turtle')
+  leonardo.shape('turtle')
 
-michelangelo.forward(random.randrange(1,100))
-leonardo.forward(random.randrange(1,100))
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
-for i in range(0, 10):
-	michelangelo.forward(random.randrange(0,10))
-	leonardo.forward(random.randrange(0,10))
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+  michelangelo.up()       
+  leonardo.up()
 
-# Part B - complete part B here
+  michelangelo.goto(-100,20)
+  leonardo.goto(-100,-20)
 
-michelangelo.down()
-for i in range(3):
-	michelangelo.forward(100)
-	michelangelo.right(120)
-michelangelo.clear()
-for i in range(4):
-	michelangelo.forward(100)
-	michelangelo.right(90)
-michelangelo.clear()
-for i in range(6):
-	michelangelo.forward(100)
-	michelangelo.right(60)
-michelangelo.clear()
-for i in range(20):
-	michelangelo.forward(100)
-	michelangelo.right(18)
-michelangelo.clear()
-for i in range(100):
-	michelangelo.forward(100)
-	michelangelo.right(3.6)
-michelangelo.clear()
+  michelangelo.forward(random.randrange(1,100))
+  leonardo.forward(random.randrange(1,100))
 
+  michelangelo.goto(-100,20)
+  leonardo.goto(-100,-20)
+
+
+def labTwoPartA():
+  '''
+   Races the two Turtles.
+   No args.
+   No return value.
+  '''
+ 
+  for i in range(0, 10):
+    michelangelo.forward(random.randrange(0,10))
+    leonardo.forward(random.randrange(0,10))
+
+  michelangelo.goto(-100,20)
+  leonardo.goto(-100,-20)
+
+  michelangelo.down()
+
+def labTwoPartB():
+'''
+  Draws shapes.
+  No args.
+  No return value.
+ '''
+  for i in range(3):
+    michelangelo.forward(100)
+    michelangelo.right(120)
+  michelangelo.clear()
+
+  for i in range(4):
+    michelangelo.forward(100)
+    michelangelo.right(90)
+  michelangelo.clear()
+
+  for i in range(6):
+    michelangelo.forward(100)
+    michelangelo.right(60)
+  michelangelo.clear()
+
+  for i in range(20):
+    michelangelo.forward(100)
+    michelangelo.right(18)
+  michelangelo.clear()
+
+  for i in range(100):
+    michelangelo.forward(100)
+    michelangelo.right(3.6)
+  michelangelo.clear()
+
+def main():
+ turtleSetUp()
+ labTwoPartA()
+ labTwoPartB()
+
+main()
 
 window.exitonclick()
