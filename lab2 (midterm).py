2be8300cb3029ea47b83dcd9c89706178d8b0b48
Ameen Kunnathu <akunnat1@binghamton.edu>
2020-10-16 20:58:52 -0400

    second commit

--- lab2 (midterm).py
+++ lab2 (midterm).py
@@ -1,8 +1,6 @@
 import turtle               
 import random
 
-#Part A
-
 window = turtle.Screen()       
 window.bgcolor('lightblue')
 
@@ -21,50 +19,51 @@ leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
-## 5. your code goes here
+def labTwo():
 
-michelangelo.forward(random.randrange(1,100))
-leonardo.forward(random.randrange(1,100))
+  michelangelo.forward(random.randrange(1,100))
+  leonardo.forward(random.randrange(1,100))
 
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+  michelangelo.goto(-100,20)
+  leonardo.goto(-100,-20)
 
-for i in range(0, 10):
-	michelangelo.forward(random.randrange(0,10))
-	leonardo.forward(random.randrange(0,10))
+  for i in range(0, 10):
+    michelangelo.forward(random.randrange(0,10))
+    leonardo.forward(random.randrange(0,10))
 
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+  michelangelo.goto(-100,20)
+  leonardo.goto(-100,-20)
 
-# Part B - complete part B here
+  michelangelo.down()
 
-michelangelo.down()
+  for i in range(3):
+    michelangelo.forward(100)
+    michelangelo.right(120)
+  michelangelo.clear()
 
-for i in range(3):
-	michelangelo.forward(100)
-	michelangelo.right(120)
-michelangelo.clear()
+  for i in range(4):
+    michelangelo.forward(100)
+    michelangelo.right(90)
+  michelangelo.clear()
 
-for i in range(4):
-	michelangelo.forward(100)
-	michelangelo.right(90)
-michelangelo.clear()
+  for i in range(6):
+    michelangelo.forward(100)
+    michelangelo.right(60)
+  michelangelo.clear()
 
-for i in range(6):
-	michelangelo.forward(100)
-	michelangelo.right(60)
-michelangelo.clear()
+  for i in range(20):
+    michelangelo.forward(100)
+    michelangelo.right(18)
+  michelangelo.clear()
 
-for i in range(20):
-	michelangelo.forward(100)
-	michelangelo.right(18)
-michelangelo.clear()
+  for i in range(100):
+    michelangelo.forward(100)
+    michelangelo.right(3.6)
+  michelangelo.clear()
 
-for i in range(100):
-	michelangelo.forward(100)
-	michelangelo.right(3.6)
-michelangelo.clear()
-
-
+def main():
+  labTwo()
+  
+main()
 
 window.exitonclick()
